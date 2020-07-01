08a230c0c78a00faf28983c0634baca6d57d9d7d
Enes Akıncı <enesakincii7@gmail.com>
2020-07-01 13:41:05 +0300

    dto for update function  added

---
+++ Article.Data/ViewModel/Dtos/ArticleUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Article.Data.ViewModel.Dtos
+{
+    public class ArticleUpdateDto
+    {
+        private const int _valueLength = 50;
+
+        public int Id { get; set; }
+        [StringLength(_valueLength)]
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
